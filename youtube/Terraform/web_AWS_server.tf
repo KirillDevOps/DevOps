2d3b8cb08694e68e4d81fc79e739319e93968c9d
Kirill Baranov <K.Baranov@aladdin.ru>
2022-08-11 16:26:03 +0300

    "tf les 12"

--- youtube/Terraform/web_AWS_server.tf
+++ youtube/Terraform/web_AWS_server.tf
@@ -1,7 +1,9 @@
 //provider "aws" {
 //  region = "ru-central-1"
 //}
-
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.my_webserver.id
+}
 resource "aws_instance" "my_webserver" {
   ami = "ami-03a71cec707bfc3d7"
   instance_type = "t3.micro"
@@ -29,13 +31,36 @@ resource "aws_instance" "my_webserver" {
     Owner = "Me"
     Project = "Terraform Lesson 8"
   }
+  lifecycle {
+    // zero downtime
+    create_before_destroy = true
+    //не убивать сервер при выполнении terraform apply
+    prevent_destroy = true
+    //игнорировать изменения в разделах:
+    ignore_changes = ["ami","user_data"]
+  }
 }
 
 resource "aws_security_group" "my_webserver-sec_group" {
   name = "Web server security group"
   description = "My security group"
 
+  dynamic "ingress" {
+    for_each = ["80","443","8080"]
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
   ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["1.2.3.4/0"]
+  }
+/*  ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
@@ -47,6 +72,7 @@ resource "aws_security_group" "my_webserver-sec_group" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+*/
   egress {
     from_port = 0
     to_port = 0
