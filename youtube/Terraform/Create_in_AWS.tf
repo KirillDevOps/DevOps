8b4f8e567484a037e3a2741c678acc80d5b6bb28
Kirill Baranov <K.Baranov@aladdin.ru>
2022-08-11 15:17:43 +0300

    "tf les 8"

--- youtube/Terraform/Create_in_AWS.tf
+++ youtube/Terraform/Create_in_AWS.tf
@@ -8,9 +8,19 @@ resource "aws_instance" "my_ubuntu" {
     count = 5
     ami = "ami-090f10efc254eaf55"
     instance_type = "t3.micro"
+    tags = {
+        name = "My Ubuntu Server"
+        Owner = "Me"
+        Project = "Terraform Lessons"
+    }
 }
 
 resource "aws_instance" "my_Amazon" {
     ami = "ami-03a71cec707bfc3d7"
-    instance_type = "t3.micro"
+    instance_type = "t3.small"
+    tags = {
+        name = "My AWS Server"
+        Owner = "Me"
+        Project = "Terraform Lessons"
+    }
 }
